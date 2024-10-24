606604005f83ffec0de146b1f6d2a616839a40b1
Miho Daniel <miho.dnl@gmail.com>
2024-10-24 19:04:00 +0200

    add contact page

--- resources/js/Components/AppFooter.vue
+++ resources/js/Components/AppFooter.vue
@@ -3,40 +3,60 @@ import { useTrans } from '@/composables/trans';
 import AppContainer from './AppContainer.vue';
 import FooterLink from './FooterLink.vue';
 
-import { CheckIcon, EnvelopeIcon, LockClosedIcon, MapPinIcon, PhoneIcon } from '@heroicons/vue/16/solid';
+import {
+    CheckIcon,
+    EnvelopeIcon,
+    LockClosedIcon,
+    MapPinIcon,
+    PhoneIcon,
+} from '@heroicons/vue/16/solid';
 
-const titleClasses = "font-semibold text-white mb-3";
+const titleClasses = 'font-semibold text-white mb-3';
 </script>
 
 <template>
-    <footer class="bg-monday text-white/70 py-6">
+    <footer class="bg-monday py-6 text-white/70">
         <AppContainer>
-            <div class="flex items-start flex-col gap-y-6 lg:flex-row lg:gap-y-0 lg:gap-x-8">
+            <div
+                class="flex flex-col items-start gap-y-6 lg:flex-row lg:gap-x-8 lg:gap-y-0"
+            >
                 <div class="flex-1">
-                    <h3 :class="titleClasses">{{ useTrans('footer.get_in_touch') }}</h3>
+                    <h3 :class="titleClasses">
+                        {{ useTrans('footer.get_in_touch') }}
+                    </h3>
 
-                    <p class="font-semibold text-white">{{ useTrans('company_name') }}</p>
+                    <p class="font-semibold text-white">
+                        {{ useTrans('company_name') }}
+                    </p>
                     <div class="text-sm">
                         <p class="text-sm">
-                            <MapPinIcon class="inline-block size-4 text-white flex-shrink-0 mr-1" />
-                            <span>{{ useTrans('footer.headquarters.label') }}: {{
-                                useTrans('footer.headquarters.address') }}
+                            <MapPinIcon
+                                class="mr-1 inline-block size-4 flex-shrink-0 text-white"
+                            />
+                            <span
+                                >{{ useTrans('footer.headquarters.label') }}:
+                                {{ useTrans('footer.headquarters.address') }}
                             </span>
                         </p>
-                        <p class="text-sm">{{ useTrans('footer.office.label') }}: {{
-                            useTrans('footer.office.address') }}</p>
-                        <p class="text-sm">{{ useTrans('footer.po_box.label') }}: {{
-                            useTrans('footer.po_box.address') }}</p>
+                        <p class="text-sm">
+                            {{ useTrans('footer.office.label') }}:
+                            {{ useTrans('footer.office.address') }}
+                        </p>
+                        <p class="text-sm">
+                            {{ useTrans('footer.po_box.label') }}:
+                            {{ useTrans('footer.po_box.address') }}
+                        </p>
 
-                        <p class="mt-6">{{ useTrans('footer.open_hours.label') }}</p>
+                        <p class="mt-6">
+                            {{ useTrans('footer.open_hours.label') }}
+                        </p>
                         <p>{{ useTrans('footer.open_hours.time') }}</p>
 
-                        <p class="mt-6 flex gap-x-1 items-center">
+                        <p class="mt-6 flex items-center gap-x-1">
                             <PhoneIcon class="size-4 text-white" />
                             <span>{{ useTrans('footer.phone') }}</span>
                         </p>
-                        <p class="flex gap-x-1 items-center">
-
+                        <p class="flex items-center gap-x-1">
                             <EnvelopeIcon class="size-4 text-white" />
                             <span>{{ useTrans('footer.email') }}</span>
                         </p>
@@ -44,35 +64,77 @@ const titleClasses = "font-semibold text-white mb-3";
                 </div>
 
                 <div class="flex-1">
-                    <h3 :class="titleClasses">{{ useTrans('footer.regulations.title') }}</h3>
+                    <h3 :class="titleClasses">
+                        {{ useTrans('footer.regulations.title') }}
+                    </h3>
 
                     <div class="text-sm">
-                        <FooterLink :icon="CheckIcon" :label="useTrans('footer.regulations.services')" href="#!" />
-                        <FooterLink :icon="CheckIcon" :label="useTrans('footer.regulations.complaints')" href="#!" />
-                        <FooterLink :icon="CheckIcon" :label="useTrans('footer.regulations.privacy_policy')"
-                            href="#!" />
-                        <FooterLink :icon="CheckIcon" :label="useTrans('footer.regulations.sustainability')"
-                            href="#!" />
-                        <FooterLink :icon="CheckIcon" :label="useTrans('footer.regulations.report')" href="#!" />
+                        <FooterLink
+                            :icon="CheckIcon"
+                            :label="useTrans('footer.regulations.services')"
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="CheckIcon"
+                            :label="useTrans('footer.regulations.complaints')"
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="CheckIcon"
+                            :label="
+                                useTrans('footer.regulations.privacy_policy')
+                            "
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="CheckIcon"
+                            :label="
+                                useTrans('footer.regulations.sustainability')
+                            "
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="CheckIcon"
+                            :label="useTrans('footer.regulations.report')"
+                            href="#!"
+                        />
                     </div>
                 </div>
 
                 <div class="flex-1">
-                    <h3 :class="titleClasses">{{ useTrans('footer.links.title') }}</h3>
+                    <h3 :class="titleClasses">
+                        {{ useTrans('footer.links.title') }}
+                    </h3>
 
                     <div class="text-sm">
-                        <FooterLink :icon="LockClosedIcon" :label="useTrans('footer.links.partners')" href="#!" />
-                        <FooterLink :icon="LockClosedIcon" :label="useTrans('footer.links.fbamsz')" href="#!" />
-                        <FooterLink :icon="LockClosedIcon" :label="useTrans('footer.links.mnb')" href="#!" />
+                        <FooterLink
+                            :icon="LockClosedIcon"
+                            :label="useTrans('footer.links.partners')"
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="LockClosedIcon"
+                            :label="useTrans('footer.links.fbamsz')"
+                            href="#!"
+                        />
+                        <FooterLink
+                            :icon="LockClosedIcon"
+                            :label="useTrans('footer.links.mnb')"
+                            href="#!"
+                        />
                     </div>
                 </div>
 
-                <div class="flex justify-end flex-shrink-0">
-                    <img src="/img/fbamsz.png" alt="" class="block size-[58px]">
+                <div class="flex flex-shrink-0 justify-end">
+                    <img
+                        src="/img/fbamsz.png"
+                        alt=""
+                        class="block size-[58px]"
+                    />
                 </div>
             </div>
 
-            <div class="text-xs mt-6">
+            <div class="mt-6 text-xs">
                 <p>{{ useTrans('footer.copyright') }}</p>
             </div>
         </AppContainer>
--- resources/js/Pages/ContactUs.vue
+++ resources/js/Pages/ContactUs.vue
@@ -1,10 +1,98 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import AppContainer from '@/Components/AppContainer.vue';
+import { onMounted } from 'vue';
+import { useTrans } from '@/composables/trans';
+
+import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/vue/24/solid';
+import L from 'leaflet';
+
+const mapSettings = {
+    x: 47.509389405424585,
+    y: 19.082308455009944,
+    zoom: 16,
+};
+
+// Function to initialize the map
+const initMap = () => {
+    const map = L.map('map', {
+        scrollWheelZoom: false,
+    }).setView([mapSettings.x, mapSettings.y], mapSettings.zoom); // Set initial view [latitude, longitude] and zoom level
+
+    // Add OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        // attribution: '© OpenStreetMap',
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+    }).addTo(map);
+
+    // Add a marker
+    L.marker([mapSettings.x, mapSettings.y]).addTo(map);
+
+    // Custom zoom behavior with Ctrl + scroll
+    map.on('wheel', (e) => {
+        if (e.ctrlKey) {
+            e.preventDefault(); // Prevent default scroll behavior
+            if (e.deltaY < 0) {
+                map.zoomIn(); // Zoom in
+            } else {
+                map.zoomOut(); // Zoom out
+            }
+        }
+    });
+};
+
+onMounted(() => {
+    initMap();
+});
 </script>
 
 <template>
     <AppLayout>
-        <h1>Contact Us</h1>
-        <p>This is the Contact Us page.</p>
+        <div id="map" class="h-[600px] w-full"></div>
+
+        <AppContainer>
+            <div class="mt-6 grid grid-cols-2 gap-x-8">
+                <div>
+                    <h2 class="text-lg font-semibold">
+                        <MapPinIcon
+                            class="mr-1 inline-block size-6 flex-shrink-0 text-black"
+                        />
+
+                        <span>{{ useTrans('company_name') }}</span>
+                    </h2>
+                    <p class="text-sm">
+                        <span
+                            >{{ useTrans('footer.headquarters.label') }}:
+                            {{ useTrans('footer.headquarters.address') }}
+                        </span>
+                    </p>
+                    <p class="text-sm">
+                        {{ useTrans('footer.office.label') }}:
+                        {{ useTrans('footer.office.address') }}
+                    </p>
+                    <p class="text-sm">
+                        {{ useTrans('footer.po_box.label') }}:
+                        {{ useTrans('footer.po_box.address') }}
+                    </p>
+                </div>
+
+                <div class="">
+                    <p>
+                        <PhoneIcon
+                            class="mr-1 inline-block size-4 flex-shrink-0 text-black"
+                        />
+                        <span>{{ useTrans('footer.phone') }}</span>
+                    </p>
+                    <p>
+                        <EnvelopeIcon
+                            class="mr-1 inline-block size-4 flex-shrink-0 text-black"
+                        />
+                        <span>{{ useTrans('footer.email') }}</span>
+                    </p>
+                </div>
+            </div>
+        </AppContainer>
     </AppLayout>
 </template>
