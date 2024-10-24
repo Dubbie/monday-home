f219086e4a3d5afa4878b1950b35a404a5852e29
Miho Daniel <miho.dnl@gmail.com>
2024-10-24 20:17:31 +0200

    style nav link component to fit theme

--- resources/js/Components/AppHeader.vue
+++ resources/js/Components/AppHeader.vue
@@ -11,47 +11,86 @@ const showingMobileMenu = ref(false);
 
 <template>
     <header>
-        <div class="mx-auto max-w-monday hidden sm:block px-6 lg:px-8">
+        <div class="mx-auto hidden max-w-monday px-6 sm:block lg:px-8">
             <div class="flex items-center justify-between">
                 <div class="flex items-center gap-x-8">
                     <Link :href="route('about-us')">
-                    <img src="/img/logo.png" alt="" />
+                        <img src="/img/logo.png" alt="" />
                     </Link>
 
                     <div class="flex items-center gap-x-1">
-                        <NavLink :href="route('about-us')">{{ useTrans('header.about_us') }}</NavLink>
-                        <NavLink :href="route('services')">{{ useTrans('header.services') }}</NavLink>
-                        <NavLink :href="route('contact-us')">{{ useTrans('header.contact') }}</NavLink>
+                        <NavLink
+                            :href="route('about-us')"
+                            :active="route().current('about-us')"
+                            >{{ useTrans('header.about_us') }}</NavLink
+                        >
+                        <NavLink
+                            :href="route('services')"
+                            :active="route().current('services')"
+                            >{{ useTrans('header.services') }}</NavLink
+                        >
+                        <NavLink
+                            :href="route('contact-us')"
+                            :active="route().current('contact-us')"
+                            >{{ useTrans('header.contact') }}</NavLink
+                        >
                     </div>
                 </div>
 
-                <AppButton :href="route('contract')" variant="primary">{{ useTrans('header.appointment') }}</AppButton>
+                <AppButton :href="route('contract')" variant="primary">{{
+                    useTrans('header.appointment')
+                }}</AppButton>
             </div>
         </div>
 
-        <div class="block sm:hidden px-6">
+        <div class="block px-6 sm:hidden">
             <div class="flex items-center justify-between">
                 <Link :href="route('about-us')">
-                <img src="/img/logo.png" alt="" />
+                    <img src="/img/logo.png" alt="" />
                 </Link>
 
                 <div>
-                    <div class="p-2 border rounded-lg shadow-sm hover:bg-black/5"
-                        @click="showingMobileMenu = !showingMobileMenu">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    <div
+                        class="rounded-lg border p-2 shadow-sm hover:bg-black/5"
+                        @click="showingMobileMenu = !showingMobileMenu"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                            />
                         </svg>
                     </div>
                 </div>
             </div>
 
             <div v-show="showingMobileMenu" class="flex flex-col">
-                <ResponsiveNavLink :href="route('about-us')">{{ useTrans('header.about_us') }}</ResponsiveNavLink>
-                <ResponsiveNavLink :href="route('services')">{{ useTrans('header.services') }}</ResponsiveNavLink>
-                <ResponsiveNavLink :href="route('contact-us')">{{ useTrans('header.contact') }}</ResponsiveNavLink>
-                <AppButton :href="route('contract')" variant="primary">{{ useTrans('header.appointment') }}</AppButton>
+                <ResponsiveNavLink
+                    :href="route('about-us')"
+                    :active="route().current('about-us')"
+                    >{{ useTrans('header.about_us') }}</ResponsiveNavLink
+                >
+                <ResponsiveNavLink
+                    :href="route('services')"
+                    :active="route().current('services')"
+                    >{{ useTrans('header.services') }}</ResponsiveNavLink
+                >
+                <ResponsiveNavLink
+                    :href="route('contact-us')"
+                    :active="route().current('contact-us')"
+                    >{{ useTrans('header.contact') }}</ResponsiveNavLink
+                >
+                <AppButton :href="route('contract')" variant="primary">{{
+                    useTrans('header.appointment')
+                }}</AppButton>
             </div>
         </div>
     </header>
--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -1,8 +1,7 @@
 <script setup>
-import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
 
-const props = defineProps({
+defineProps({
     href: {
         type: String,
         required: true,
@@ -11,16 +10,17 @@ const props = defineProps({
         type: Boolean,
     },
 });
-
-const classes = computed(() =>
-    props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out',
-);
 </script>
 
 <template>
-    <Link :href="href" :class="classes">
+    <Link
+        :href="href"
+        class="rounded-lg px-2 py-1.5 font-semibold"
+        :class="{
+            'text-monday': active,
+            'text-zinc-800 hover:bg-zinc-100': !active,
+        }"
+    >
         <slot />
     </Link>
 </template>
