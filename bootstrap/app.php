8033b7122b890917e57544c453e696f672bf04ec
Miho Daniel <miho.dnl@gmail.com>
2024-10-27 10:04:28 +0100

    add cookie consent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\SetLocale::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            \Spatie\CookieConsent\CookieConsentMiddleware::class,
         ]);
 
         $middleware->api(append: [
